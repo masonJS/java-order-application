8f1949bebf149376627b64a14f1ce80bf3939876
minwoo <mwoodev526@gmail.com>
2022-11-26 21:19:37 +0900

    파트너 Service 구현

---
+++ src/main/java/com/ddd/order/domain/partner/PartnerServiceImpl.java
@@ -0,0 +1,46 @@
+package com.ddd.order.domain.partner;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PartnerServiceImpl implements PartnerService {
+    private final PartnerStore partnerStore;
+    private final PartnerReader partnerReader;
+
+    @Override
+    @Transactional
+    public PartnerInfo registerPartner(PartnerCommand command) {
+        val initPartner = command.toEntity();
+        val partner = partnerStore.store(initPartner);
+        return new PartnerInfo(partner);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public PartnerInfo getPartnerInfo(String partnerToken) {
+        Partner partner = partnerReader.getPartner(partnerToken);
+        return new PartnerInfo(partner);
+    }
+
+    @Override
+    @Transactional
+    public PartnerInfo enablePartner(String partnerToken) {
+        Partner partner = partnerReader.getPartner(partnerToken);
+        partner.enable();
+        return new PartnerInfo(partner);
+    }
+
+    @Override
+    @Transactional
+    public PartnerInfo disablePartner(String partnerToken) {
+        Partner partner = partnerReader.getPartner(partnerToken);
+        partner.disable();
+        return new PartnerInfo(partner);
+    }
+}

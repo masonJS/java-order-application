c86938a996828549d47d57b9eefcdaf13feca566
minwoo <mwoodev526@gmail.com>
2022-11-26 21:19:53 +0900

    Common 예외 클래스 구현

---
+++ src/main/java/com/ddd/order/common/exception/EntityNotFoundException.java
@@ -0,0 +1,15 @@
+package com.ddd.order.common.exception;
+
+
+import com.ddd.order.common.response.ErrorCode;
+
+public class EntityNotFoundException extends BaseException {
+
+    public EntityNotFoundException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+}
---
+++ src/main/java/com/ddd/order/common/exception/InvalidParamException.java
@@ -0,0 +1,23 @@
+package com.ddd.order.common.exception;
+
+
+import com.ddd.order.common.response.ErrorCode;
+
+public class InvalidParamException extends BaseException {
+
+    public InvalidParamException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public InvalidParamException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public InvalidParamException(String errorMsg) {
+        super(errorMsg, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public InvalidParamException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
+    }
+}
